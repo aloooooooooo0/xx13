eb9259f8b550692e2dde74a4491f511012f11470
1-МД-4 Зыков Даниил <102769793+aloooooooooo0@users.noreply.github.com>
2022-06-09 23:28:34 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import telebot
 from telebot import types
 import menuBot
 from menuBot import Menu
-#import DZ   домашнее задание от первого урока
+# import DZ   домашнее задание от первого урока
 import SECRET
 import botGames
 import random
+import wiki
+
 bot = telebot.TeleBot(SECRET.TELEGRAM_TOKEN)
 import fun
+
 
 # -----------------------------------------------------------------------
 # Класс цветов - не понимаю, они просто не работают
@@ -25,9 +28,9 @@ class color:
 # Функция, обрабатывающая команду start
 
 @bot.message_handler(commands="start")
-def command(message): #CAACAgIAAxkBAAEE1HZikA-X4PsOYWekoxPv8voOobWn5wACGxQAAk9VcEn_Ed1DKZ44WSQE
-    chat_id = message.chat.id #
-    txt_message = f"Добро пожаловать, {message.from_user.first_name}!" + "   Я - " + "BotLiki" + ", бот созданный чтобы быть подопытным кроликом.\n\nСписок моих возможностей приведён снизу:" + "\n - могу прислать анекдот" + "\n - могу прислать аниме GIF" + "\n - могу прислать номер" + "\n - могу прислать не существующего челоека" + "\n - могу прислать курс биткоина" + "\n - могу прислать фильм" + "\n - могу прислать сериал" + "\n - могу прислать погоду" + "\n - могу прислать книгу для прочтения" + "\n - во мне есть дз" + "\n - во мне есть незавершённый Джарвис" + "\n - во мне есть простенькие игры" + "\n - реагирую на команды /help, /online и \n/start"
+def command(message):  # CAACAgIAAxkBAAEE1HZikA-X4PsOYWekoxPv8voOobWn5wACGxQAAk9VcEn_Ed1DKZ44WSQE
+    chat_id = message.chat.id  #
+    txt_message = f"Добро пожаловать, {message.from_user.first_name}!" + "   Я - " + "BotLiki" + ", бот созданный чтобы быть подопытным кроликом.\n\nСписок моих возможностей приведён снизу:" + "\n - могу прислать анекдот" + "\n - могу прислать аниме GIF" + "\n - могу прислать номер" + "\n - могу прислать не существующего челоека" + "\n - могу прислать курсы валют" + "\n - могу прислать фильм" + "\n - могу прислать сериал" + "\n - могу прислать погоду" + "\n - могу прислать книгу для прочтения" + "\n - во мне есть дз" + "\n - во мне есть незавершённый Джарвис" + "\n - во мне есть простенькие игры" + "\n - реагирую на команды /help, /online и \n/start"
     bot.send_message(chat_id, text=txt_message, reply_markup=Menu.getMenu(chat_id, "Главное меню").markup)
     bot.send_sticker(chat_id, "CAACAgIAAxkBAAEE5J1imHGEvDM4v0_hoel0d71gIBBQPAAC_hYAAufTaUmqys3-dkKlhSQE")
 
@@ -50,8 +53,11 @@ def help_command(message):
     markup = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton(text='Спросить у разработчика', url='https://t.me/alo0ooooo')
     markup.add(btn1)
-    bot.send_message(chat_id, ' - Автор: Зыков Даниил\n\n' + ' - Это первый созданный его руками бот.\n' + ' - Бот является проектной работой по предмету "Алоритмизация и програмирование"\n' + ' - Наставником на верный путь был Швец Андрей.', reply_markup=markup)
+    bot.send_message(chat_id,
+                     ' - Автор: Зыков Даниил\n\n' + ' - Это первый созданный его руками бот.\n' + ' - Бот является проектной работой по предмету "Алоритмизация и програмирование"\n' + ' - Наставником на верный путь был Швец Андрей.',
+                     reply_markup=markup)
     bot.send_sticker(chat_id, "CAACAgIAAxkBAAEE5JRimG3700XiIPkZ4f9o80MyNzpN-AAC4RUAAu_7iEn9jgO1a0v3MSQE")
+
 
 # -----------------------------------------------------------------------
 # Функция, обрабатывающая команду online
@@ -63,7 +69,8 @@ def command(message):
     bot.send_message(chat_id, "Активные пользователи чат-бота:")
     for el in menuBot.Users.activeUsers:
         bot.send_message(chat_id, menuBot.Users.activeUsers[el].getUserHTML(), parse_mode='HTML')
-    #bot.send_sticker(chat_id, "CAACAgIAAxkBAAEE1HZikA-X4PsOYWekoxPv8voOobWn5wACGxQAAk9VcEn_Ed1DKZ44WSQE")
+    # bot.send_sticker(chat_id, "CAACAgIAAxkBAAEE1HZikA-X4PsOYWekoxPv8voOobWn5wACGxQAAk9VcEn_Ed1DKZ44WSQE")
+
 
 # -----------------------------------------------------------------------
 # Получение не сообщений от юзера
@@ -72,6 +79,7 @@ def command(message):
 def get_messages(message):
     chat_id = message.chat.id
     bot.send_message(chat_id, "Это " + message.content_type + ", мне запретили отвечать на сообщения этого типа.")
+
 
 # -----------------------------------------------------------------------
 # Получение сообщений от юзера
@@ -99,7 +107,8 @@ def get_text_messages(message):
             exec(module + ".get_text_messages(bot, cur_user, message)")
 
     else:  # ======================================= случайный текст
-        bot.send_message(chat_id, text="Я не могу распознать вашу команду, мой хозяин не добавлял подобного функцианала: " + ms_text)
+        bot.send_message(chat_id,
+                         text=" - Я не могу распознать вашу команду, мой хозяин не добавлял подобного функцианала: " + ms_text)
         menuBot.goto_menu(bot, chat_id, "Главное меню")
 
 
@@ -108,7 +117,6 @@ def get_text_messages(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-
     chat_id = call.message.chat.id
     message_id = call.message.id
     cur_user = menuBot.Users.getUser(chat_id)
@@ -119,6 +127,7 @@ def callback_worker(call):
     menu = tmp[0] if len(tmp) > 0 else ""
     cmd = tmp[1] if len(tmp) > 1 else ""
     par = tmp[2] if len(tmp) > 2 else ""
+
 
 #    if menu == "GameRPSm":
 #        botGames.callback_worker(bot, cur_user, cmd, par, call)
@@ -136,65 +145,65 @@ def callback_worker(call):
 # -----------------------------------------------------------------------
 # Игра Числа
 
-storage = dict()
-
-def init_storage(user_id):
-    storage[user_id] = dict(attempt=None, random_digit=None)
-
-
-def set_data_storage(user_id, key, value):
-    storage[user_id][key] = value
-
-
-def get_data_storage(user_id, chat_id):
-    return storage[user_id]
-
-
-def digit_games(bot, message, chat_id):
-    init_storage(message.chat.id)
-
-    attempt = 5
-    set_data_storage(message.chat.id, "attempt", attempt)
-
-    bot.send_message(message.chat.id, f'Игра "угадай число"!\nКоличество попыток: {attempt}')
-
-    random_digit = random.randint(1, 10)
-
-    set_data_storage(message.chat.id, "random_digit", random_digit)
-
-    bot.send_message(message.chat.id, 'Готово! Загадано число от 1 до 10!')
-    bot.send_message(message.chat.id, 'Ну давай, введи число, попробуй угадай;)')
-    bot.register_next_step_handler(message, process_digit_step)
-
-
-def process_digit_step(bot, message):
-    chat_id = message.chat.id
-    user_digit = message.text
-
-    if not user_digit.isdigit():
-        msg = bot.reply_to(message, 'Ты ввел не цифры. Вводи только цифры!')
-        bot.register_next_step_handler(msg, process_digit_step)
-        return
-
-    attempt = get_data_storage(chat_id)["attempt"]
-    random_digit = get_data_storage(chat_id)["random_digit"]
-
-    if int(user_digit) == random_digit:
-        bot.send_sticker(chat_id, "CAACAgIAAxkBAAEE5btimROuB5eos0VdeyZn2J5RXUg_WAAC-AsAAqydAAFIkwFAkULAXYYkBA")
-        bot.send_message(chat_id, f'Ура! Ты угадал число! Это была цифра {random_digit}')
-        init_storage(chat_id)
-        return
-    elif attempt > 1:
-        attempt -= 1
-        set_data_storage(chat_id, "attempt", attempt)
-        bot.send_message(chat_id, f'Неверно, осталось попыток: {attempt}')
-        bot.register_next_step_handler(message, process_digit_step)
-    else:
-        bot.send_message(chat_id, 'Твои попытки закончились, ты проиграл!')
-        init_storage(chat_id)
-        return
-
-
+# storage = dict()
+#
+# def init_storage(user_id):
+#     storage[user_id] = dict(attempt=None, random_digit=None)
+#
+#
+# def set_data_storage(user_id, key, value):
+#     storage[user_id][key] = value
+#
+#
+# def get_data_storage(user_id, chat_id):
+#     return storage[user_id]
+#
+#
+# def digit_games(bot, message, chat_id):
+#     init_storage(message.chat.id)
+#
+#     attempt = 5
+#     set_data_storage(message.chat.id, "attempt", attempt)
+#
+#     bot.send_message(message.chat.id, f'Игра "угадай число"!\nКоличество попыток: {attempt}')
+#
+#     random_digit = random.randint(1, 10)
+#
+#     set_data_storage(message.chat.id, "random_digit", random_digit)
+#
+#     bot.send_message(message.chat.id, 'Готово! Загадано число от 1 до 10!')
+#     bot.send_message(message.chat.id, 'Ну давай, введи число, попробуй угадай;)')
+#     bot.register_next_step_handler(message, process_digit_step)
+#
+#
+# def process_digit_step(bot, message):
+#     chat_id = message.chat.id
+#     user_digit = message.text
+#
+#     if not user_digit.isdigit():
+#         msg = bot.reply_to(message, 'Ты ввел не цифры. Вводи только цифры!')
+#         bot.register_next_step_handler(msg, process_digit_step)
+#         return
+#
+#     attempt = get_data_storage(chat_id)["attempt"]
+#     random_digit = get_data_storage(chat_id)["random_digit"]
+#
+#     if int(user_digit) == random_digit:
+#         bot.send_sticker(chat_id, "CAACAgIAAxkBAAEE5btimROuB5eos0VdeyZn2J5RXUg_WAAC-AsAAqydAAFIkwFAkULAXYYkBA")
+#         bot.send_message(chat_id, f'Ура! Ты угадал число! Это была цифра {random_digit}')
+#         init_storage(chat_id)
+#         return
+#     elif attempt > 1:
+#         attempt -= 1
+#         set_data_storage(chat_id, "attempt", attempt)
+#         bot.send_message(chat_id, f'Неверно, осталось попыток: {attempt}')
+#         bot.register_next_step_handler(message, process_digit_step)
+#     else:
+#         bot.send_message(chat_id, 'Твои попытки закончились, ты проиграл!')
+#         init_storage(chat_id)
+#         return
+#
+#
 
 
 bot.polling(none_stop=True, interval=0)
--- wiki.py
+++ wiki.py
@@ -1,45 +1,64 @@
 #---------------------------------------
 # Сам код wiki
 
-import telebot
-import wikipedia, re
-import SECRET
-bot = telebot.TeleBot(SECRET.TELEGRAM_TOKEN)
-
-wikipedia.set_lang("ru")
-
-def get_wiki(message):
-    try:
-        ny = wikipedia.page(message)
-        wikitext=ny.content[:1000]
-        wikimas=wikitext.split('.')
-        wikimas = wikimas[:-1]
-        wikitext2 = ''
-        for x in wikimas:
-            if not('==' in x):
-                if(len((x.strip()))>3):
-                   wikitext2=wikitext2+x+'.'
-            else:
-                break
-        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
-        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
-        wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
-        return wikitext2
-    except Exception:
-        return 'В энциклопедии нет информации об этом'
-
-#def my_jar(bot, message):
-#   bot.send_message(message.chat.id, get_wiki(message.text))
-
-def my_input(bot, chat_id, txt, ResponseHandler):
-    message = bot.send_message(chat_id, text=txt)
-    bot.register_next_step_handler(message, ResponseHandler)
+# import telebot
+# import wikipedia, re
+# import SECRET
+# bot = telebot.TeleBot(SECRET.TELEGRAM_TOKEN)
 #
-# @bot.message_handler(commands=["asistent"])
-# def jar(bot, chat_id):
-#     my_input(bot, chat_id, "Джарвис находится на стадии разработки?", jar_ResponseHandler)
+# wikipedia.set_lang("ru")
+#
+# def get_wiki(message):
+#     try:
+#         ny = wikipedia.page(message)
+#         wikitext = ny.content[:1000]
+#         wikimas = wikitext.split('.')
+#         wikimas = wikimas[:-1]
+#         wikitext2 = ''
+#         for r in wikimas:
+#             if not('==' in r):
+#                 if(len((r.strip()))>3):
+#                    wikitext2 = wikitext2+r+'.'
+#             else:
+#                 break
+#         wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
+#         wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
+#         wikitext2 = re.sub('\{[^\{\}]*\}', '', wikitext2)
+#         bot.send_message(chat_id = message.chat.id, text = wikitext2)
+#     except Exception as e:
+#         bot.send_message(chat_id = message.chat.id, text ='В энциклопедии нет информации об этом')
 #
 #
+
 #
-# def jar_ResponseHandler(message, bot, chat_id):
-#     bot.send_message(chat_id, text=f"{get_wiki(message.text)}")
+# def my_input(bot, chat_id, txt, ResponseHandler):
+#     message = bot.send_message(chat_id, text=txt)
+#     bot.register_next_step_handler(message, ResponseHandler)
+# #
+# # @bot.message_handler(commands=["asistent"])
+# # def jar(bot, chat_id):
+# #     my_input(bot, chat_id, "Джарвис находится на стадии разработки?", jar_ResponseHandler)
+# #
+# #
+# #
+# # def jar_ResponseHandler(message, bot, chat_id):
+# #     bot.send_message(chat_id, text=f"{get_wiki(message.text)}")
+#
+# def get_text_messages(bot, cur_user, message):
+#     chat_id = message.chat.id
+#     ms_text = message.text
+#
+#     if ms_text == "Поиск":
+#         #bot.send_message(message.chat.id, "Джарвис находится на стадии разработки")
+#         #my_jar(bot, chat_id, text=f"Отправьте мне любое слово, и я найду его значение на Wikipedia", get_wiki())
+#         # bot.send_message(message.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
+#         #
+#         # bot.send_message(message.chat.id, get_wiki(message.text))
+#         jar_ResponseHandler = lambda message: bot.send_message(chat_id, text=get_wiki(message))
+#         my_input(bot, chat_id, "Отправьте мне любое слово, и я найду его значение на Wikipedia", jar_ResponseHandler)
+# #    elif ms_text == "Угадай число":
+# #        bot.send_message(text=digit_games(bot, message, chat_id))
+# #        bot.send_message(digit_games(message))
+# #        bot.send_message(bot, chat_id, digit_games())
+# #def my_jar(bot, message):
+# #   bot.send_message(message.chat.id, get_wiki(message.text))
